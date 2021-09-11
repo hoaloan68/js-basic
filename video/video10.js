f7c51e6cbb559404247fa135a22eb875664c6a4c
Thanh <hoaloanmin58@gmail.com>
2021-09-11 11:19:19 +0700

    video so 10,11

---
+++ video/video10.js
@@ -0,0 +1,11 @@
+console.log('hello worLd from HTML');
+
+let a = 5;
+let b = '5';
+
+console.log(a == b, a === b);
+
+let arr = ['Mu', 'Mancity', 'Chelsea'];
+for (let i = 0; i < arr.length; i++) {
+  console.log('check variable i: ', arr[i]);
+}
